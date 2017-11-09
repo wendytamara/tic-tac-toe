99d356f1b15b5068c83446a7d653d3ac977d0118
wendytamara <anacarlavegam@gmail.com>
2017-11-09 00:56:27 -0500

    avance js

---
+++ js/app.js
@@ -0,0 +1,27 @@
+window.onload = function() {
+  var board = document.querySelector(".board-js");
+
+board.addEventListener("click", onCellClick);
+function onCellClick(event){
+    var target = event.target;
+  var dataset = target.dataset;
+  /*console.log(dataset); */
+ if (dataset && dataset.row) {
+   console.log("pos", dataset.row, dataset.column);
+ }
+}
+
+
+board.addEventListener("click", addX);
+  };
+var centinel = false;
+
+function addX (event){
+  if (centinel) {
+  event.target.textContent ="X";
+  centinel = false;
+} else {
+    event.target.textContent ="O";
+    centinel = true;
+}
+}
